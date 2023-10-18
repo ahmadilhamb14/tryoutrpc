b1d892ade9bdadb3153b35c497280177af480356
Stefany Frans Sarira <114038304+stefanysarira@users.noreply.github.com>
2023-10-18 22:13:37 +0800

    Update sidebar.blade.php

---
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -0,0 +1,21 @@
+<div class="container-wrapper">
+        <div class="sidebar" style="border-radius: 10px;">
+            <div class="container py-2" style="background-color: #FFC107;" >
+              <h4>Menu Utama</h4>
+            </div>
+            <div class="menu" >
+              <a href="#">
+                <i class="fa-solid fa-house"></i>
+                Home</a>
+              <a href="#" >
+                  <i class="fa-solid fa-user"></i>
+                   Profile</a>
+              <a href="#">
+                <i class="fa-solid fa-pen-to-square"></i>
+                Tryout</a>
+              <a href="#">
+                <i class="fa-solid fa-trophy"></i>
+                Result</a>
+            </div>
+          </div>
+    </div>
