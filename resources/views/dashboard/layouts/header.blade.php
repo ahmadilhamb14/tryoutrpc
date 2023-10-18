7fa52359e9dcaa3d11edabf800c1807bccd8c796
wiryaaods <wiryasusetiyo@gmail.com>
2023-10-18 21:41:02 +0800

    navbar

---
+++ resources/views/dashboard/layouts/header.blade.php
@@ -0,0 +1,25 @@
+<div>
+        <nav class="navbar" style="background-color: #424242; height: max-content;" >
+            <div class="container-fluid" >
+              <a class="navbar-brand " href="#" style="font-size:30sp; font-weight: 500; margin-left: 12px; color: #FFC107"  >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20" class="bi bi-image =" viewBox="0 0 16 16" style="margin-right: 10px;">
+                    <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                    <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
+                  </svg>
+                Ranu<span style="color: aliceblue;">PrimaCollege</span>
+              </a>
+              <div class="dropdown" style="margin-right: 10px;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16" s>
+                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.
+                    001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
+                  </svg>
+                <button class="btn btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" style="background-color: #424242; border: none; box-shadow: none;font-size: 30sp; font-weight: 500; color: #FFC107">
+                    Nama Pengguna
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#" style="font-size: 30sp; font-weight: 500;">Logout</a></li>
+                  </ul>
+            </div>
+            </div>
+          </nav>
+    </div>
