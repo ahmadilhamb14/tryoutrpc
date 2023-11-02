4c9d0a4a3e8f808908ee94b56f9a6a3e232207ba
Djulizah <djul.b74123@gmail.com>
2023-11-02 08:10:29 +0800

    add subtest and tryout seeder

--- database/seeders/SubTestSeeder.php
+++ database/seeders/SubTestSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SubTestSeeder extends Seeder
 {
@@ -14,6 +15,65 @@ class SubTestSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('sub_tests')->insert([
+            'id' => 1,
+            'id_tryout' => 1,
+            'subtes' => 'Penalaran Umum',
+            'timer' => 30,
+        ]);
+        DB::table('sub_tests')->insert([
+            'id' => 2,
+            'id_tryout' => 1,
+            'subtes' => 'Penalaran Matematika',
+            'timer' => 30,
+        ]);
+        DB::table('sub_tests')->insert([
+            'id' => 3,
+            'id_tryout' => 1,
+            'subtes' => 'Membaca Menulis',
+            'timer' => 20,
+        ]);
+        DB::table('sub_tests')->insert([
+            'id' => 4,
+            'id_tryout' => 1,
+            'subtes' => 'Kuantitatif',
+            'timer' => 20,
+        ]);
+        DB::table('sub_tests')->insert([
+            'id' => 5,
+            'id_tryout' => 1,
+            'subtes' => 'Literasi Bahasa Indonesia',
+            'timer' => 30,
+        ]);
+        DB::table('sub_tests')->insert([
+            'id' => 6,
+            'id_tryout' => 1,
+            'subtes' => 'Literasi Bahasa Inggris',
+            'timer' => 30,
+        ]);
+        DB::table('sub_tests')->insert([
+            'id' => 7,
+            'id_tryout' => 1,
+            'subtes' => 'Pengetahuan Umum',
+            'timer' => 20,
+        ]);
+        DB::table('sub_tests')->insert([
+            'id' => 8,
+            'id_tryout' => 2,
+            'subtes' => 'Tes Wawasan Kebangsaan',
+            'timer' => 30,
+        ]);
+        DB::table('sub_tests')->insert([
+            'id' => 9,
+            'id_tryout' => 2,
+            'subtes' => 'Tes Intelegensia Umum',
+            'timer' => 30,
+        ]);
+        DB::table('sub_tests')->insert([
+            'id' => 10,
+            'id_tryout' => 2,
+            'subtes' => 'Tes Karakteristik Pribadi',
+            'timer' => 20,
+        ]);
     }
 }
